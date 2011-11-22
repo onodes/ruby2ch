366aeec32b82335662ebab91e5e1a17539f9acaf
Daichi ONODERA <onodes@onod.es>
2011-11-23 01:34:17 +0900

    test

--- lib/ruby2ch.rb
+++ lib/ruby2ch.rb
@@ -1,6 +1,10 @@
+$:.unshift(File.dirname(__FILE__)) unless 
+$:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
 require "ruby2ch/version"
-require 'ruby2ch/thre'
-module Ruby2ch
+require "ruby2ch/news"
+
+#module Ruby2ch
   # Your code goes here...
   
-end
+#end
